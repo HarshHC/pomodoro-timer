bd926b9133871c5b19af642ffe3c4bb0e6f188f7
jeisper <71713717+jeisper@users.noreply.github.com>
2021-01-28 16:49:35 +0000

    Add chakra Library Add Tasks Componets

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Header from './components/Header.js';
 import Timer from './components/Timer';
+import Tasks from './components/Tasks';
+
 
 function App() {
   return (
     <div>
       <Header />
       <Timer />
+      <Tasks />
+
     </div>
   );
 }
