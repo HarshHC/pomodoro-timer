6c428114e00d1d7017b33dc0bf3e088ffbaaf4dc
Neil Gibson <1neilgibson1@gmail.com>
2021-02-15 19:49:39 +0000

    Fixed pause for session timer

--- src/components/Timer/RunningTimer.js
+++ src/components/Timer/RunningTimer.js
@@ -21,6 +21,7 @@ function RunningTimer(props) {
   const sessionCountdownHandler = () => {
     if (sTime <= 0) {
       props.setIsRunning(false);
+      setTime((time) => props.breakMins * 60);
       props.setMode(BREAK);
       props.setBreakIsRunning(true);
     }
@@ -29,7 +30,8 @@ function RunningTimer(props) {
   //This checks if the time for the break is complete
   const breakCountdownHandler = () => {
     if (bTime <= 0) {
-      props.setBreakIsRunning(false);
+      props.setBreakIsRunning(!props.breakIsRunning);
+      setTime(props.sessionMins * 60);
       props.setMode(SESSION);
       props.setIsRunning(true);
     }
@@ -40,14 +42,15 @@ function RunningTimer(props) {
   //This use effect uses set interval to count the session time we set down to 0, all calcs done inside
   //re-renders every time the value in isRunning changes
   useEffect(() => {
+    sTime = time - 2;
     if (props.isRunning) {
       const id = window.setInterval(() => {
         sessionCountdownHandler();
         setSessionSeconds(sTime % 60);
         setMins(Math.floor(sTime / 60));
+        setTime((time) => time - 1);
         sTime--;
       }, 1000);
-
       return () => window.clearInterval(id);
     }
   }, [props.isRunning]);
@@ -55,11 +58,13 @@ function RunningTimer(props) {
   //This use effect uses set interval to count the break time we set down to 0, all calcs done inside
   //re-renders every time the value in breakIsRunning changes
   useEffect(() => {
+    bTime = time - 2;
     if (props.breakIsRunning) {
       const id2 = window.setInterval(() => {
         breakCountdownHandler();
         setBSeconds(bTime % 60);
         setBMins(Math.floor(bTime / 60));
+        setTime((time) => time - 1);
         bTime--;
       }, 1000);
 
